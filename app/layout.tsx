64f91ccae7c3be98bc28fe64e90394c69c1d8566
Ashish Jha <ashishxyzjha@gmail.com>
2026-06-13 22:53:03 +0530

    folder structure change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Headers from "./components/Headers";
+import Headers from "@/components/Headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
