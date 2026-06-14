fa0a93abd2ff86c533f14a854aba2c34f182bffe
Ashish Jha <ashishxyzjha@gmail.com>
2026-06-14 18:47:37 +0530

    implement fixes

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -68,7 +68,18 @@ const CandlestickChart = ({
 
     const series = chart.addSeries(CandlestickSeries, getCandlestickConfig());
 
-    series.setData(convertOHLCData(ohlcData));
+    const convertedToSeconds = ohlcData.map(
+      (item) =>
+        [
+          Math.floor(item[0] / 1000),
+          item[1],
+          item[2],
+          item[3],
+          item[4],
+        ] as OHLCData,
+    );
+
+    series.setData(convertOHLCData(convertedToSeconds));
     chart.timeScale().fitContent();
 
     chartRef.current = chart;
@@ -86,7 +97,7 @@ const CandlestickChart = ({
       chartRef.current = null;
       candleSeriesRef.current = null;
     };
-  }, [height]);
+  }, [height, period]);
 
   useEffect(() => {
     if (!candleSeriesRef.current) return;
